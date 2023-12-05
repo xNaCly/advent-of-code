0ebc85b7c67e834c1905bb1b2d76598f1bd990b5
xnacly <47723417+NaCly@users.noreply.github.com>
2023-12-05 10:30:42 +0100

    2023/01: part2

--- 2023/src/day1.rs
+++ 2023/src/day1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn part1(lines: Vec<String>) -> u32 {
     lines
         .iter()
@@ -7,10 +9,37 @@ fn part1(lines: Vec<String>) -> u32 {
                 .collect::<Vec<char>>()
         })
         .map(|digits| {
+            println!("{:?}", digits);
             (digits.first().unwrap().to_digit(10).unwrap() * 10)
                 + digits.last().unwrap().to_digit(10).unwrap()
         })
         .sum()
+}
+
+fn part2(lines: Vec<String>) -> u32 {
+    let name_to_digit: HashMap<&str, &str> = HashMap::from([
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ]);
+    part1(
+        lines
+            .iter()
+            .map(|l| {
+                let mut l = l.to_string();
+                for (key, value) in name_to_digit.iter() {
+                    l = l.replace(key, &format!("{key}{value}{key}"));
+                }
+                l
+            })
+            .collect(),
+    )
 }
 
 #[cfg(test)]
@@ -24,15 +53,7 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet",
         );
-        assert_eq!(
-            day1::part1(
-                testin
-                    .lines()
-                    .map(|l| String::from(l))
-                    .collect::<Vec<String>>()
-            ),
-            142
-        )
+        assert_eq!(day1::part1(testin.lines().map(String::from).collect()), 142)
     }
 
     #[test]
@@ -40,10 +61,46 @@ treb7uchet",
         use crate::day1;
         use std::fs::File;
         use std::io::{self, prelude::*, BufReader};
-        let file = File::open("./src/input.txt").unwrap();
+        let file = File::open("./day01.txt").unwrap();
         let r = BufReader::new(file);
         let lines = r.lines().map(|l| l.unwrap()).collect::<Vec<_>>();
         let out = day1::part1(lines);
+        println!("result={}", out)
+    }
+
+    #[test]
+    fn day1_test_part2() {
+        use crate::day1;
+        let testin = String::from(
+            "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen",
+        );
+        assert_eq!(
+            day1::part2(
+                testin
+                    .lines()
+                    .filter(|l| !l.is_empty())
+                    .map(String::from)
+                    .collect()
+            ),
+            281
+        )
+    }
+
+    #[test]
+    fn day1_test_real_part2() {
+        use crate::day1;
+        use std::fs::File;
+        use std::io::{self, prelude::*, BufReader};
+        let file = File::open("./day01.txt").unwrap();
+        let r = BufReader::new(file);
+        let lines = r.lines().map(|l| l.unwrap()).collect::<Vec<_>>();
+        let out = day1::part2(lines);
         println!("result={}", out)
     }
 }
