207bbd3fb8d4ea992c56ec2aa475a16011b8dfcd
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-12-01 01:46:52 +0100

    2024: setup

---
+++ 2024/src/day1.rs
@@ -0,0 +1,24 @@
+fn part1(lines: Vec<String>) -> u32 {
+    0
+}
+
+fn part2(lines: Vec<String>) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod day1 {
+    use crate::day1::*;
+
+    #[test]
+    fn part1() {}
+
+    #[test]
+    fn part1_real() {}
+
+    #[test]
+    fn part2() {}
+
+    #[test]
+    fn part2_real() {}
+}
